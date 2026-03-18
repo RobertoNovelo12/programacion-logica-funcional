16fc7239a3326f01316f0082ce85f855bf0f7c73
RobertoNovelo12 <robertonovelo98@gmail.com>
2026-03-18 15:26:25 -0500

    Actividad1

---
+++ 01_print.py
@@ -0,0 +1,25 @@
+from os import system
+
+if system("clear") != 0: system("cls")
+
+
+print("Hola, crayola!")
+
+print('Esto tambien funciona con una comilla')
+
+print("Python", "es", "genial")
+
+print("Python", "es", "brutal", sep = "-")
+
+print("Esto se imprime", end = "\n")
+
+print("En una linea")
+
+print(42)
+
+print('Esto es una "pulgada" dentro de un string de comillas simples')
+
+print("Esto es una \"pulgada\" demtro de un string de comillas dobles")
+
+print("""Esto es una "pulgada" dentro de un sring con triple comillas""")
+
---
+++ 02_types.py
@@ -0,0 +1,35 @@
+from os import system
+
+if system("clear") != 0: system("cls")
+
+print("int:")
+
+print(type(10))
+print(type(0))
+print(type(-5))
+print(type(7091237489127389471273487123947120374))
+
+print("float:")
+print(type(3.14))
+print(type(1.0))
+print(type(1e3))
+
+
+print("complex")
+print(type(1 + 2))
+
+print("str:")
+print(type("Hola"))
+print(type(""))
+print(type("123"))
+print(type("""
+Multilinea
+"""))
+
+print("bool")
+print(type(True))
+print(type(False))
+print(type(1<2))
+
+print("NoneTtype: ")
+print(type(None))
